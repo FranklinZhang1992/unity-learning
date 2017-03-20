977b096ef29962df7d8be6b171764f65c5f99102
FranklinZhang1992 <461847112@qq.com>
2017-03-20 09:38:12 +0800

    modify: c demo: for_test.c

--- c/for_test.c
+++ c/for_test.c
@@ -3,9 +3,16 @@
 // gcc for_test.c -o for_test.out -std=c99
 int main(int argc, char const *argv[])
 {
-  for (int i = 0; i < 5; i++); printf("Hello\n");
-  for (int i = 0; i < 5; i++) {
-    printf("World\n");
+  // for (int i = 0; i < 5; i++); printf("Hello\n");
+  // for (int i = 0; i < 5; i++) {
+  //   printf("World\n");
+  // }
+  int i = 0;
+  for (;;) {
+    i ++;
+    if (i == 3)
+        break
+    printf("i = %d\n", i);
   }
   return 0;
 }
