e9809f4efaee5eedd0d2948d75c032ee762356d2
FranklinZhang1992 <461847112@qq.com>
2017-09-27 09:43:01 +0800

    Added ruby demo: file_manager

---
+++ ruby/demo_tests/file_manager.rb
@@ -0,0 +1,58 @@
+require 'tempfile'
+require 'pathname'
+
+class FileManager
+
+    def initialize(encoding)
+        Encoding.find(encoding)
+        @encoding = encoding
+    end
+
+    def file_encoding(file)
+        output = %x{file --mime-encoding #{file}}.chomp
+        regexp = Regexp.new("#{file}: (\\S+)")
+        matched = output.match(regexp)
+        encoding = matched[1] if matched
+    end
+
+    def read(file, &blk)
+        File.open(file, "rb:#{file_encoding(file)}") do |f|
+            while line = f.gets
+                line = line.nil? ? nil : line.encode(@encoding)
+                blk.call(line)
+            end
+        end
+    end
+
+    def write(file, content)
+        temp_file = Tempfile.new("#{File.basename(file)}", Pathname.new(File.dirname(file)).realpath, :encoding => @encoding)
+        begin
+            temp_file.puts content
+            temp_file.flush
+            FileUtils.cp(temp_file.path, file)
+        ensure
+            temp_file.close!
+        end
+    end
+end
+
+file_manager = FileManager.new("UTF-8")
+tmp_file = "/tmp/test"
+
+content = <<EOF
+ααββ
+ののの
+测试
+aaa
+bbb
+ccc
+EOF
+
+file_manager.write(tmp_file, content)
+
+content = ""
+file_manager.read(tmp_file) do |line|
+    content << line
+end
+puts content
+puts "content encoding is #{content.encoding}"
