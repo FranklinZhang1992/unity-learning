93d19ab718aa5660837891a32afab2c5e8bd68b2
FranklinZhang1992 <461847112@qq.com>
2018-01-01 19:23:14 +0800

    TaskScheduler: Modify test cases

--- java/TaskScheduler/src/com/demo/test/Main.java
+++ java/TaskScheduler/src/com/demo/test/Main.java
@@ -1,18 +1,50 @@
 package com.demo.test;
 
+import java.util.Date;
+import java.util.List;
+
 import com.demo.service.CrontabParser;
 import com.demo.utils.Util;
 
 public class Main {
 
-    private static void test() {
-        String trigger = "20 23 * */3 *";
+    protected static void printList(String title, List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        for (Integer i : list) {
+            sb.append(i);
+            sb.append(",");
+        }
+        System.out.println(title);
+        System.out.println("    " + sb.toString());
+    }
+
+    protected static void printParser(CrontabParser parser) {
+        List<Integer> minuteList = parser.getMinuteField().getFieldList();
+        printList("minute", minuteList);
+        List<Integer> hourList = parser.getHourField().getFieldList();
+        printList("hour", hourList);
+        List<Integer> dayOfMonthList = parser.getDayOfMonthField().getFieldList();
+        printList("day-of-month", dayOfMonthList);
+        List<Integer> monthList = parser.getMonthField().getFieldList();
+        printList("month", monthList);
+        List<Integer> dayOfWeekList = parser.getDayOfWeekField().getFieldList();
+        printList("day-of-week", dayOfWeekList);
+    }
+
+    protected static void inspect() {
+        String trigger = "20 18 * */7 *";
+        System.out.println("Current time is: " + Util.getFormatedTime(new Date()));
         CrontabParser parser = new CrontabParser(trigger);
-        System.out.println(Util.getFormatedTime(parser.next()));
+        printParser(parser);
+        Date nextDate = parser.next();
+
+        System.out.println(Util.getFormatedTime(nextDate));
+
     }
 
     public static void main(String[] args) {
-        test();
+
+        inspect();
     }
 
 }
