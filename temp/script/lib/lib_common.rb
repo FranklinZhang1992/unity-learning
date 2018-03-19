d4249bace315c7acc05983f44abaf35c12e4a1c2
FranklinZhang1992 <461847112@qq.com>
2018-03-19 17:14:26 +0800

    [NONRESOLVE] refine scripts

--- temp/script/lib/lib_common.rb
+++ temp/script/lib/lib_common.rb
@@ -2,12 +2,75 @@ require 'pathname'
 
 class CommandError < RuntimeError ;
 
+class UserRepoConfig
+    attr :user, :repo_name, :repo_url
+    def initialize(configs)
+        @user = configs.shift
+        @repo_name = configs.shift
+        @repo_url = configs.shift
+    end
+end
+
+class MajorBranchConfig
+    attr :val
+    def initialize(configs)
+        @val = configs.shift
+    end
+end
+
+class BuildServerConfig
+    attr :val
+    def initialize(configs)
+        @val = configs.shift
+    end
+end
+
+class BaseDirectoryConfig
+    attr :val
+    def initialize(configs)
+        @val = configs.shift
+    end
+end
+
+class ConfigLoader
+
+    attr :user_repo_configs, :major_branch_config, :build_server_config, :base_dir_config
+
+    def initialize
+        @user_repo_configs = []
+        IO.readlines("config").each do |line|
+            configs = line.split(" ")
+            key = configs.shift
+            case key
+            when "user-repo"
+                @user_repo_configs << UserRepoConfig.new(configs)
+            when "major-branch"
+                @major_branch_config = MajorBranchConfig.new(configs)
+            when "build-server"
+                @build_server_config = BuildServerConfig.new(configs)
+            when "base"
+                @base_dir_config = BaseDirectoryConfig.new(configs)
+            else
+                puts "unknown config key #{key}"
+            end
+        end
+    end
+end
+
+def config_loader() $loader ||= ConfigLoader.new end
+
 def current_user
     %x{whoami}.chomp
 end
 
 def get_pool_base
-    File.join("/developer", current_user)
+    base = begin
+               config_loader.base_dir_config.val
+           rescue
+               puts "no pool base defined, use default pool base: /developer"
+               "/developer"
+           end
+    File.join(base, current_user)
 end
 
 def exec_cmd(cmd, verbose=true)
@@ -21,40 +84,23 @@ def exec_cmd(cmd, verbose=true)
 end
 
 def build_server
-    IO.read("build_server").strip
+    config_loader.build_server_config.val
 rescue
-    nil
+    puts "no build serer defined, use default build server: shanghai12"
+    "shanghai12"
 end
 
-class UserRepoConfig
-    attr :user, :repo_name, :repo_url
-    def initialize(configs)
-        @user = configs.shift
-        @repo_name = configs.shift
-        @repo_url = configs.shift
-    end
+def major_branch
+    config_loader.major_branch_config.val
+rescue
+    puts "no major branch defined, use default major branch: master"
+    "master"
 end
 
-class ConfigLoader
-
-    attr :user_repo_configs
-
-    HANDLERS = {
-        "user-repo" => [UserRepoConfig, @user_repo_configs = []]
-    }
-
-    def initialize
-        @user_repo_configs = []
-        IO.readlines("../lib/script.conf").each do |line|
-            configs = line.split(" ")
-            key = configs.shift
-            hanlder = HANDLERS[key]
-            case key
-            when "user-repo"
-                @user_repo_configs << UserRepoConfig.new(configs)
-            else
-                puts "unknown config key #{key}"
-            end
-        end
-    end
+def checkout_branch(branch)
+    exec_cmd("git checkout #{branch}")
+    output = %x{git branch | grep "* #{branch}"}
+    puts output
+    $?.success? or raise CommandError, "failed to checkout to branch #{branch}"
+    branch
 end
