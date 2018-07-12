1260789e6b33633f92b8b89d761fda14eda1b013
FranklinZhang1992 <461847112@qq.com>
2018-07-12 10:37:44 +0800

    cache

--- java/CustomizedBase64/src/CustomizedBase32.java
+++ java/CustomizedBase64/src/CustomizedBase32.java
@@ -1,3 +1,4 @@
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
@@ -34,7 +35,7 @@ public class CustomizedBase32 {
 			byte[] dst = new byte[len];
 			int ret = encode0(src, dst);
 			if (ret != dst.length)
-				return Arrays.copyOf(dst, ret);
+				throw new RuntimeException("invalid return length");
 			return dst;
 		}
 
@@ -42,36 +43,82 @@ public class CustomizedBase32 {
 			char[] base32 = toBase32;
 			int sp = 0;
 			int dp = 0;
-			int stop = src.length;
+			int end = src.length;
+			int sl = (end - sp) / 5 * 5;
+			
 			int round = 1;
-			while (sp < stop) {
-				switch (round) {
-				case 1:
-					dst[dp++] = (byte) base32[(src[sp++] >>> 3) & 0x1f];
-					round++;
-					break;
-				case 2:
-					dst[dp++] = (byte) base32[((src[sp - 1] & 0x7) << 2) | (src[sp] >>> 6)];
-					round++;
-					break;
-				case 3:
-					dst[dp++] = (byte) base32[(src[sp++] & 0x3e) >>> 1];
-					round++;
-					break;
-				case 4:
-					dst[dp++] = (byte) base32[((src[sp - 1] & 0x1) << 7) | src[sp] >>> 4];
-					round++;
-					break;
-				case 5:
-				case 6:
-				case 7:
-				case 8:
-				default:
-					throw new RuntimeException("invalid round");
-				}
+
+			while (sp < sl) {
+				int bits0 = (src[sp++] & 0xff) <<  8 |
+				            (src[sp++] & 0xff);
+			int bits1 = (src[sp++] & 0xff) << 16 |
+				        (src[sp++] & 0xff) <<  8 |
+				        (src[sp++] & 0xff);
+
+			dst[dp++] = (byte)base32[(bits0 >>> 11) & 0x1f];
+            dst[dp++] = (byte)base32[(bits0 >>> 6) & 0x1f];
+            dst[dp++] = (byte)base32[(bits0 >>> 1) & 0x1f];
+            dst[dp++] = (byte)base32[((bits0 & 0x1) << 4) | ((bits1 >>> 20) & 0xf)];
+            dst[dp++] = (byte)base32[(bits1 >>> 15) & 0x1f];
+            dst[dp++] = (byte)base32[(bits1 >>> 10) & 0x1f];
+            dst[dp++] = (byte)base32[(bits1 >>> 5) & 0x1f];
+            dst[dp++] = (byte)base32[bits1 & 0x1f];
 			}
+			
+			
+//			while (sp < len) {
+//				switch (round) {
+//				case 1:
+//					dst[dp++] = (byte) base32[(src[sp++] >>> 3) & 0x1f];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 2:
+//					dst[dp++] = (byte) base32[((src[sp - 1] & 0x7) << 2) | (src[sp] >>> 6)];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 3:
+//					dst[dp++] = (byte) base32[(src[sp++] & 0x3e) >>> 1];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 4:
+//					dst[dp++] = (byte) base32[((src[sp - 1] & 0x1) << 4) | src[sp++] >>> 4];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 5:
+//					dst[dp++] = (byte) base32[((src[sp - 1] & 0xf) << 1) | src[sp] >>> 7];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 6:
+//					dst[dp++] = (byte) base32[(src[sp++] & 0x7c) >>> 2];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 7:
+//					dst[dp++] = (byte) base32[((src[sp - 1] & 0x3) << 3) | src[sp] >>> 5];
+//					round++;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				case 8:
+//					dst[dp++] = (byte) base32[src[sp++] & 0x1f];
+//					round = 1;
+//					System.out.println("round = " + round + ", dp = " + dp + ", sp = " + sp);
+//					break;
+//				default:
+//					throw new RuntimeException("invalid round");
+//				}
+//			}
 
 			return dp;
+		}
+
+		public String encodeToString(byte[] src) {
+			byte[] encoded = encode(src);
+			return new String(encoded);
 		}
 	}
 
@@ -100,7 +147,7 @@ public class CustomizedBase32 {
 		}
 
 		public byte[] decode(String src) {
-			return decode(src.getBytes(StandardCharsets.ISO_8859_1));
+			return decode(src.getBytes());
 		}
 
 		private int outLength(byte[] src, int sp, int sl) {
