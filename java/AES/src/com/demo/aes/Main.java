720bf649a4334e93ad9810ca2422709649dfd274
FranklinZhang1992 <461847112@qq.com>
2018-07-12 15:24:19 +0800

    Refine test case for AES

--- java/AES/src/com/demo/aes/Main.java
+++ java/AES/src/com/demo/aes/Main.java
@@ -30,12 +30,23 @@ public class Main {
 		System.out.println("decrypted string: " + decryptedStr);
 	}
 
+	protected static void test3(String plainText) throws Exception {
+		System.out.println("original string: " + plainText);
+		String key = generateKey();
+		System.out.println("Generated key: " + key);
+		String encryptedStr = aes.encrypt(key, plainText);
+		System.out.println("ecrypted string (Base64): " + encryptedStr);
+		String decryptedStr = aes.decrypt(key, encryptedStr);
+		System.out.println("decrypted string (Base64): " + decryptedStr);
+		encryptedStr = aes.encryptWithCustomizedBase32(key, plainText);
+		System.out.println("ecrypted string (Base32): " + encryptedStr);
+		decryptedStr = aes.decryptWithCustomizedBase32(key, encryptedStr);
+		System.out.println("decrypted string (Base32): " + decryptedStr);
+	}
+
 	public static void main(String[] args) throws Exception {
 		String plainText = "Hello world!";
-		test2(plainText);
-		test2(plainText);
-		test2(plainText);
-		test2(plainText);
+		test3(plainText);
 	}
 
 }
