e56722ad4dd621010c07610f4400b30421c1c31b
FranklinZhang1992 <461847112@qq.com>
2018-08-09 20:08:13 +0800

    Modified java JwdDemo

--- java/JwtDemo/src/com/demo/jwt/JwtManager.java
+++ java/JwtDemo/src/com/demo/jwt/JwtManager.java
@@ -3,57 +3,35 @@ package com.demo.jwt;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Date;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtManager {
-	private static final long MAX_INTERVAL = 1000 * 60 * 30;
-	private RSAPublicKey publicKey;
-	private RSAPrivateKey privateKey;
-	private static JwtManager instance;
+    private static final long MAX_INTERVAL = 1000 * 60 * 30;
+    private static JwtManager instance;
 
-	public synchronized static JwtManager getInstance() throws NoSuchAlgorithmException {
-		if (instance == null) {
-			instance = new JwtManager();
-		}
-		return instance;
-	}
+    private KeyPair keyPair;
 
-	public JwtManager() throws NoSuchAlgorithmException {
-		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
-		generator.initialize(2048, new FixedSecureRandom());
-		KeyPair keyPair = generator.generateKeyPair();
+    public synchronized static JwtManager getInstance() throws NoSuchAlgorithmException {
+        if (instance == null) {
+            instance = new JwtManager();
+        }
+        return instance;
+    }
 
-		publicKey = (RSAPublicKey) keyPair.getPublic();
-		privateKey = (RSAPrivateKey) keyPair.getPrivate();
-	}
+    public JwtManager() throws NoSuchAlgorithmException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048, new FixedSecureRandom());
+        keyPair = generator.generateKeyPair();
 
-	public String generate() throws NoSuchAlgorithmException {
-		Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
-		String token = JWT.create().withAudience("userA").withIssuedAt(new Date()).sign(algorithm);
-		return token;
-	}
+    }
 
-	public boolean verify(String token) {
-		try {
-			Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
-			JWTVerifier verifier = JWT.require(algorithm).withIssuer("userA").build();
-			DecodedJWT jwt = verifier.verify(token);
-			Date issuedDate = jwt.getIssuedAt();
-			if ((System.currentTimeMillis() - issuedDate.getTime()) < MAX_INTERVAL) {
-				System.err.println("Expired token");
-				return true;
-			}
-		} catch (JWTVerificationException exception) {
-			System.err.println("Invalid token");
-		}
-		return false;
-	}
+    public String generate() {
+        return Jwts.builder().setSubject("Joe").signWith(keyPair.getPrivate()).compact();
+    }
+
+    // public boolean verify(String token) {
+    // }
 }
