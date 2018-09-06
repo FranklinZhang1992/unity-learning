0d2dd16c452624ffc0718078cf412b97415c3bcf
FranklinZhang1992 <461847112@qq.com>
2018-09-06 23:58:33 +0800

    More servers of SpringCloudDemo added

---
+++ java/SpringCloudDemo/microservice-book/src/main/java/com/microservice/book/microservicebook/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.book.microservicebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
