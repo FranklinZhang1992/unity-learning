b9de740cf46d603c95b59e1e50e4346b7e83af6f
FranklinZhang1992 <461847112@qq.com>
2018-09-10 22:52:56 +0800

    Save uncommited changes

--- java/SpringCloudDemo/microservice-book/src/main/java/com/microservice/book/microservicebook/controller/BookController.java
+++ java/SpringCloudDemo/microservice-book/src/main/java/com/microservice/book/microservicebook/controller/BookController.java
@@ -1,13 +1,19 @@
 package com.microservice.book.microservicebook.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.microservice.book.microservicebook.model.Book;
 
 @RestController
-@
+@RequestMapping("book")
 public class BookController {
 
-    @GetMapping("/book")
-    public Book getBookByAuthorId()
+    @GetMapping("/author/id/{id}")
+    public Book getBookByAuthorId(@PathVariable Long id) {
+        
+    }
 }
